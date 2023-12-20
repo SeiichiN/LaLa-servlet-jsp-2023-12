bfdcd8dd1a4ec1223f7d67b4639b3b7e9c6ad43e
SeiichiN <billie175@gmail.com>
2023-12-20 16:24:23 +0900

    サーブレット１日め 12/20 p92

---
+++ example/src/main/java/p91/FirstServlet.java
@@ -0,0 +1,25 @@
+package p91;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet1")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<p><a href='/example/Servlet2'>Servlet2</a></p>");
+		out.println("</body></html>");
+	}
+
+}
---
+++ example/src/main/java/p91/SecondServlet.java
@@ -0,0 +1,24 @@
+package p91;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet2")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>よう来たのぉ</h1>");
+		out.println("</body></html>");
+	}
+
+}
