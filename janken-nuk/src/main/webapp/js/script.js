91dd3470b96dae93297da09751de2957922219fb
SeiichiN <billie175@gmail.com>
2023-12-28 16:29:28 +0900

    janken-nuk script.js modified.

--- janken-nuk/src/main/webapp/js/script.js
+++ janken-nuk/src/main/webapp/js/script.js
@@ -7,3 +7,14 @@ for (let i = 0; i < hands.length; i++) {
 		console.log('click');
 	})
 }
+
+// 東京(130000)の予報を取得
+let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json";
+
+fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(weather) {
+        console.log(weather);
+    });
